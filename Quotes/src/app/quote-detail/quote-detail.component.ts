4579b3c4142fae0b7ff6254b42103d4bf8b98276
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-02-08 03:38:18 +0300

    adding the event emitter logic

--- Quotes/src/app/quote-detail/quote-detail.component.ts
+++ Quotes/src/app/quote-detail/quote-detail.component.ts
@@ -10,7 +10,11 @@ import { Quote } from '../quote';
 export class QuoteDetailComponent implements OnInit {
 
   @Input() quote!: Quote;
-  
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  quoteComplete(complete: boolean) {
+    this.isComplete.emit(complete);
+  }
 
   constructor() {};
 

4290d2ca9e97271d9858d87ffee59c1d81fe372c
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-02-08 05:08:03 +0300

    Creating the quote-form component

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { ArchiveDirective } from './archive.directive';
 import { DateCountDownPipe } from './date-count-down.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { DateCountDownPipe } from './date-count-down.pipe';
     QuoteComponent,
     QuoteDetailComponent,
     ArchiveDirective,
-    DateCountDownPipe
+    DateCountDownPipe,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
